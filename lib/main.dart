4994df41d3c4623395d935a350866ca88444c7b8
tambong <80495675+tambongslade@users.noreply.github.com>
2024-10-06 12:08:00 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Diceioe'),
+          title: Text('Dice'),
           backgroundColor: Colors.red,
         ),
         body: DiceApp(),
